67f934d480f855e338c60501b2cb9521414acc75
theEvilReaper <steffenwx@gmail.com>
2025-01-20 10:58:30 +0100

    Update dependency usage and yeet Jitpack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,14 @@ dependencyResolutionManagement {
         create("libs") {
             version("kotlin", "2.1.0")
             version("minestom", "1.5.1")
-            version("junit", "5.11.4")
             library("microtus-bom", "net.onelitefeather.microtus", "bom").versionRef("minestom")
             library("jetbrains.annotation", "org.jetbrains", "annotations").withoutVersion()
             library("microtus", "net.onelitefeather.microtus", "Microtus").withoutVersion()
             library("dartpoet", "dev.themeinerlp", "dartpoet").version("0.0.1-SNAPSHOT")
             library("guava", "com.google.guava", "guava").version("33.4.0-jre")
-            library("jgit", "org.eclipse.jgit", "org.eclipse.jgit").version("6.10.0.202406032230-r")
-            library("junit.jupiter", "org.junit.jupiter", "junit-jupiter").versionRef("junit")
-            library("junit.jupiter.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("jgit", "org.eclipse.jgit", "org.eclipse.jgit").version("7.1.0.202411261347-r")
+            library("junit.jupiter", "org.junit.jupiter", "junit-jupiter").withoutVersion()
+            library("junit.jupiter.engine", "org.junit.jupiter", "junit-jupiter-engine").withoutVersion()
             plugin("kotlin", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
         }
     }
