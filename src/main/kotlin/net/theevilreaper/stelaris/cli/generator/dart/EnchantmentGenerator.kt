336a7ea0f9d5229785b2149f624cef2b51213711
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-16 21:52:46 +0200

    Fix enchantment generation

--- src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/EnchantmentGenerator.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/EnchantmentGenerator.kt
@@ -1,18 +1,25 @@
 package net.theevilreaper.stelaris.cli.generator.dart
 
+import net.kyori.adventure.text.TranslatableComponent
 import net.minestom.server.MinecraftServer
 import net.minestom.server.item.enchant.Enchantment
+import net.theevilreaper.dartpoet.DartFile
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.clazz.ClassSpec
+import net.theevilreaper.dartpoet.constructor.ConstructorSpec
 import net.theevilreaper.dartpoet.enum.EnumEntrySpec
 import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
 import net.theevilreaper.stelaris.cli.generator.BaseGenerator
+import net.theevilreaper.stelaris.cli.generator.dart.util.CLASS_PROPERTIES
+import net.theevilreaper.stelaris.cli.generator.dart.util.CONSTRUCTOR_PARAMETERS
 import net.theevilreaper.stelaris.cli.util.StringHelper
 import java.nio.file.Path
 
 /**
- * The [EnchantmentGenerator] contains the logic to map the data from an [EnchantmentWrapper] into a dart enum
- * which contains all available enchantments.
- * @constructor Sets the basic values for the generation
- * @version 1.0
+ * The [EnchantmentGenerator] is responsible for generating the Dart enum for Minecraft enchantments.
+ * It retrieves all enchantments from the Minecraft server and maps them to an enum format.
+ *
+ * @since 1.0.0
  * @author theEvilReaper
  */
 class EnchantmentGenerator : BaseGenerator(
@@ -23,10 +30,14 @@ class EnchantmentGenerator : BaseGenerator(
     private val defaultLevel: Int = 1
 
     override fun generate(javaPath: Path) {
-        val enchantmentData = MinecraftServer.getEnchantmentRegistry().values()
+        val enchantmentData: MutableCollection<Enchantment> = MinecraftServer.getEnchantmentRegistry().values()
         val properties = mutableSetOf<EnumEntrySpec>()
-        //enchantmentData.forEach { properties.add(mapEnchantmentToEnumProperty(it)) }
-        /*val enumClass = ClassSpec.enumClass(className)
+
+        enchantmentData.forEach { enchantment ->
+            properties.add(mapEnchantmentToEnumProperty(enchantment))
+        }
+
+        val enumClass = ClassSpec.enumClass(className)
             .enumProperties(*properties.toTypedArray())
             .properties(*CLASS_PROPERTIES)
             .constructor {
@@ -40,33 +51,38 @@ class EnchantmentGenerator : BaseGenerator(
             .doc("The file is generated. Don't change anything here")
             .type(enumClass)
             .build()
-        enumFile.write(javaPath)*/
+        enumFile.write(javaPath)
     }
 
     /**
-     * Maps the given [EnchantmentWrapper] into a [EnumPropertySpec].
-     * @param enchantment the given enchantment
-     * @return the generated property
+     * Maps the [Enchantment] to an [EnumEntrySpec] which can be used in the generated enum.
+     * @param enchantment the enchantment to map
+     * @return the mapped [EnumEntrySpec]
      */
-    private fun mapEnchantmentToEnumProperty(enchantment: Enchantment): EnumEntrySpec? {
-        /*val enchantmentEntry = enchantment.registry()
-        return EnumEntrySpec.builder(enchantmentEntry!!.namespace.path())
-            .parameter(EnumParameterSpec.positional(
-                "%C",
-                StringHelper.mapDisplayName(enchantmentEntry.namespace.path())
-            ))
-            .parameter(EnumParameterSpec.positional("%C", "MEEPO"))
+    private fun mapEnchantmentToEnumProperty(enchantment: Enchantment): EnumEntrySpec {
+        val translatable = enchantment.description() as TranslatableComponent
+        val enchantmentName = translatable.key().substringAfterLast(".")
+        return EnumEntrySpec.builder(enchantmentName)
+            .parameter(
+                EnumParameterSpec.positional(
+                    "%C",
+                    StringHelper.mapDisplayName(enchantmentName)
+                )
+            )
             .parameter(EnumParameterSpec.positional("%L", defaultLevel))
-            .parameter(EnumParameterSpec.positional("%L", 1)) //TODO: Fix me
-            .build()*/
-        return null
+            .parameter(EnumParameterSpec.positional("%L", enchantment.maxLevel()))
+            .build()
     }
 
     /**
-     * Returns the name from [EnchantmentWrapper] which is used as identifier.
-     * @return the given name
+     * Returns the name of the generator.
+     * @return the name
      */
     override fun getName() = "EnchantmentGenerator"
 
-    override fun isExperimental() = false
+    /**
+     * Returns whether the generator is experimental or not.
+     * @return false, since this generator is not experimental
+     */
+    override fun isExperimental(): Boolean = false
 }
