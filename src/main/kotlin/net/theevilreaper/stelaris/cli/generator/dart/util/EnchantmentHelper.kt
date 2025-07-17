7a4271d390d45686be2ab2651c8947c20304a342
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-17 17:32:28 +0200

    Remove unused definitions

--- src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/util/EnchantmentHelper.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/util/EnchantmentHelper.kt
@@ -6,14 +6,12 @@ import net.theevilreaper.dartpoet.property.PropertySpec
 
 internal val CLASS_PROPERTIES: Array<PropertySpec> = arrayOf(
     PropertySpec.builder("name", String::class).modifier { DartModifier.FINAL }.build(),
-    /*PropertySpec.builder("category", String::class).modifier { DartModifier.FINAL }.build(),*/
     PropertySpec.builder("minLevel", Integer::class).modifier { DartModifier.FINAL }.build(),
     PropertySpec.builder("maxLevel", Integer::class).modifier { DartModifier.FINAL }.build()
 )
 
 internal val CONSTRUCTOR_PARAMETERS: Array<ParameterSpec> = arrayOf(
     ParameterSpec.positional("name").build(),
-    /*ParameterSpec.positional("category").build(),*/
     ParameterSpec.positional("minLevel").build(),
     ParameterSpec.positional("maxLevel").build()
 )
