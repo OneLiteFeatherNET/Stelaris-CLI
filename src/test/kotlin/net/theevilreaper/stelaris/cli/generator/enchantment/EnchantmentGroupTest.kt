fbe6ce980934a06fc837a5b193874bbee77e7e4c
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-17 16:00:36 +0200

    Fix naming

--- src/test/kotlin/net/theevilreaper/stelaris/cli/generator/enchantment/EnchantmentGroupTest.kt
+++ src/test/kotlin/net/theevilreaper/stelaris/cli/generator/enchantment/EnchantmentGroupTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class nchantmentGroupTest {
+class EnchantmentGroupTest {
 
     @Test
     fun `test group count should be 18`() {
