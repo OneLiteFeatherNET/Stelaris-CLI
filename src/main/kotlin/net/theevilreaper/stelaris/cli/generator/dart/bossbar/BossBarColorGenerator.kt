815bed38b66847cea622092f7eb84a952bccf31b
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-17 16:16:30 +0200

    Create for each bossbar related generation an own generator implementation

---
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/bossbar/BossBarColorGenerator.kt
@@ -0,0 +1,60 @@
+package net.theevilreaper.stelaris.cli.generator.dart.bossbar
+
+import net.kyori.adventure.bossbar.BossBar
+import net.theevilreaper.dartpoet.DartFile
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.clazz.ClassSpec
+import net.theevilreaper.dartpoet.constructor.ConstructorSpec
+import net.theevilreaper.dartpoet.enum.EnumEntrySpec
+import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
+import net.theevilreaper.stelaris.cli.generator.BaseGenerator
+import net.theevilreaper.stelaris.cli.generator.dart.util.DEFAULT_PARAMETERS
+import net.theevilreaper.stelaris.cli.generator.dart.util.DEFAULT_PROPERTIES
+import net.theevilreaper.stelaris.cli.util.StringHelper
+import java.nio.file.Path
+
+class BossBarColorGenerator : BaseGenerator(
+    className = "BossBarColor",
+    packageName = "bossbar",
+) {
+
+    override fun generate(javaPath: Path) {
+        val file = DartFile.Companion.builder(packageName)
+            .type(
+                ClassSpec.Companion.enumClass(className)
+                    .apply {
+                        BossBar.Color.entries.forEach { color ->
+                            enumProperty(
+                                EnumEntrySpec.Companion.builder(color.name.lowercase())
+                                    .parameter(
+                                        EnumParameterSpec.Companion.positional(
+                                            "%C",
+                                            StringHelper.mapDisplayName(color.name)
+                                        )
+                                    )
+                                    .parameter(
+                                        EnumParameterSpec.Companion.positional(
+                                            "%C",
+                                            color.name.uppercase()
+                                        )
+                                    )
+                                    .build()
+                            )
+                        }
+                    }
+                    .properties(*DEFAULT_PROPERTIES)
+                    .constructor(
+                        ConstructorSpec.Companion.builder(className)
+                            .modifier(DartModifier.CONST)
+                            .parameters(*DEFAULT_PARAMETERS)
+                            .build()
+                    )
+            )
+            .build()
+        file.write(javaPath)
+    }
+
+    override fun getName(): String = "BossBarColorGenerator"
+
+    override fun isExperimental(): Boolean = false
+}
---
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/bossbar/BossBarFlagGenerator.kt
@@ -0,0 +1,60 @@
+package net.theevilreaper.stelaris.cli.generator.dart.bossbar
+
+import net.kyori.adventure.bossbar.BossBar
+import net.theevilreaper.dartpoet.DartFile
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.clazz.ClassSpec
+import net.theevilreaper.dartpoet.constructor.ConstructorSpec
+import net.theevilreaper.dartpoet.enum.EnumEntrySpec
+import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
+import net.theevilreaper.stelaris.cli.generator.BaseGenerator
+import net.theevilreaper.stelaris.cli.generator.dart.util.DEFAULT_PARAMETERS
+import net.theevilreaper.stelaris.cli.generator.dart.util.DEFAULT_PROPERTIES
+import net.theevilreaper.stelaris.cli.util.StringHelper
+import java.nio.file.Path
+
+class BossBarFlagGenerator : BaseGenerator(
+    "BossBarFlag",
+    "bossbar",
+) {
+
+    override fun generate(javaPath: Path) {
+        val file = DartFile.builder("bossbar_flag")
+            .type(
+                ClassSpec.enumClass(className)
+                    .apply {
+                        BossBar.Overlay.entries.forEach { overlay ->
+                            enumProperty(
+                                EnumEntrySpec.builder(overlay.name.lowercase())
+                                    .parameter(
+                                        EnumParameterSpec.positional(
+                                            "%C",
+                                            StringHelper.mapDisplayName(overlay.name)
+                                        )
+                                    )
+                                    .parameter(
+                                        EnumParameterSpec.positional(
+                                            "%C",
+                                            overlay.name.uppercase()
+                                        )
+                                    )
+                                    .build()
+                            )
+                        }
+                    }
+                    .properties(*DEFAULT_PROPERTIES)
+                    .constructor(
+                        ConstructorSpec.builder(className)
+                            .modifier(DartModifier.CONST)
+                            .parameters(*DEFAULT_PARAMETERS)
+                            .build()
+                    )
+            )
+            .build()
+        file.write(javaPath)
+    }
+
+    override fun getName(): String = "BossBarFlagGenerator"
+
+    override fun isExperimental(): Boolean = false
+}
