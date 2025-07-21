61123f36345ac163e50cb339939e89273d2efcf7
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-21 22:40:04 +0200

    Disable a test

--- src/test/kotlin/net/theevilreaper/stelaris/cli/generator/GeneratorRegistryTest.kt
+++ src/test/kotlin/net/theevilreaper/stelaris/cli/generator/GeneratorRegistryTest.kt
@@ -1,10 +1,12 @@
 package net.theevilreaper.stelaris.cli.generator
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class GeneratorRegistryTest {
 
+    @Disabled("Need a rework")
     @Test
     fun testGetGenerators() {
         val generatorRegistry = GeneratorRegistry()
