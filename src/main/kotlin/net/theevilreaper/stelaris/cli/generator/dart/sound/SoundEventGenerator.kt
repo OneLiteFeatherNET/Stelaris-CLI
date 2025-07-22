6a9523566dbeeab7de4ba87bdf89a883de176a6c
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-22 11:29:55 +0200

    Add key to the sound generation and update name mapping

--- src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/sound/SoundEventGenerator.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/sound/SoundEventGenerator.kt
@@ -11,8 +11,6 @@ import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
 import net.theevilreaper.dartpoet.property.PropertySpec
 import net.theevilreaper.stelaris.cli.generator.BaseGenerator
 import net.theevilreaper.stelaris.cli.util.StringHelper
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -21,61 +19,60 @@ class SoundEventGenerator : BaseGenerator(
     packageName = "sound",
 ) {
 
-    private val soundLogger: Logger = LoggerFactory.getLogger(SoundEventGenerator::class.java)
-
     override fun generate(javaPath: Path) {
         val folder = javaPath.resolve("sound")
         if (!Files.exists(folder)) {
             Files.createDirectory(folder)
         }
         val soundEntries = SoundEvent.values()
-        val mappedEntries: Map<SoundType, List<SoundEvent>> = soundEntries.mapNotNull { sound ->
-            val soundKey: Key = sound.key()
-            val soundDataString: List<String> = soundKey.value().split(".")
-            val soundType: SoundType? = SoundType.fromType(soundDataString.first())
+        val mappedEntries = SoundHelper.mapSoundEvents(soundEntries)
 
-            if (soundType == null) {
-                soundLogger.warn("Sound event ${soundKey.value()} does not have a valid type, skipping.")
-                null
-            } else {
-                soundType to sound
-            }
-        }.groupBy({ it.first }, { it.second })
-
+        val enumFiles = mutableListOf<DartFile>()
         mappedEntries.forEach { (key, value) ->
-            
+            val className = "${key.type.replaceFirstChar { it.uppercase() }}Sound"
+            val fileName = "${key.type}_sound"
+
+            val enumClass = ClassSpec.enumClass(className)
+                .apply {
+                    value.forEach { it ->
+                        val soundKey: Key = it.key()
+                        val soundData = soundKey.value().split(".")
+                        val soundType = soundData[1]
+                        val variant = soundData.last()
+                        val soundTypeTitle = SoundHelper.refactorSoundData(soundType)
+                        val soundVariant = SoundHelper.refactorSoundData(variant)
+                        val displayName = StringHelper.mapDisplayName("$soundTypeTitle $soundVariant")
+                        enumProperty(
+                            EnumEntrySpec.builder("${soundType}_$variant")
+                                .parameter(EnumParameterSpec.positional("%C", displayName))
+                                .parameter(EnumParameterSpec.positional("%C", it.key().asString()))
+                                .build()
+                        )
+                    }
+                }
+                .property(
+                    PropertySpec.builder("name", String::class).modifier(DartModifier.FINAL).build()
+                )
+                .property(
+                    PropertySpec.builder("key", String::class).modifier(DartModifier.FINAL).build()
+                )
+                .constructor(
+                    ConstructorSpec.builder(className)
+                        .modifier(DartModifier.CONST)
+                        .build()
+                )
+                .build()
+            val file = DartFile.builder(fileName)
+                .doc("Generated class for the sound sources. Don't edit this file manually")
+                .type(enumClass)
+                .build()
+            enumFiles.add(file)
         }
 
 
-        val enumClass = ClassSpec.enumClass(className)
-            .apply {
-                val entries = SoundEvent.values()
-                entries.forEach {
-                    val soundKey: Key = it.key()
-                    println("Adding sound event: ${soundKey.value()}")
-                    val splittedSoundData = soundKey.value().split(".")
-                    enumProperty(
-                        EnumEntrySpec.builder(it.name())
-                            .parameter(EnumParameterSpec.positional("%C", StringHelper.mapDisplayName(it.name())))
-                            .build()
-                    )
-                }
-            }
-            .property(PropertySpec.builder("name", String::class).modifier(DartModifier.FINAL).build())
-            .constructor(
-                ConstructorSpec.builder(className)
-                    .modifier(DartModifier.CONST)
-                    .build()
-            )
-            .build()
-        val file = DartFile.builder(packageName)
-            .doc("Generated class for the sound sources. Don't edit this file manually")
-            .type(enumClass)
-            .build()
-        file.write(folder)
+        // Write all enum files to the folder
+        enumFiles.forEach { it.write(folder) }
     }
 
     override fun getName(): String = "SoundTypeGenerator"
-
-    override fun isExperimental() = false
 }
