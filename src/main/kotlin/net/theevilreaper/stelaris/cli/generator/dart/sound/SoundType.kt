af7bf2c16b9a2c72dfded8031a7bf5c8e17aa068
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-22 11:36:47 +0200

    Add documentation

--- src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/sound/SoundType.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/sound/SoundType.kt
@@ -1,5 +1,16 @@
 package net.theevilreaper.stelaris.cli.generator.dart.sound
 
+/**
+ * The [SoundType] enum is a group representing different categories of sounds in the game.
+ * Each type corresponds to a specific category of sound events, such as blocks, entities, music, items, and ambient sounds.
+ *
+ * **This enum doesn't reflect the actual sound categories from the game with 100% accuracy.**
+ *
+ * @property type The string representation of the sound type.
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 enum class SoundType(val type: String) {
 
     BLOCK("block"),
@@ -11,6 +22,11 @@ enum class SoundType(val type: String) {
 
     companion object {
 
+        /**
+         * Finds a [SoundType] by its type string.
+         * @param type the type string to search for.
+         * @return the [SoundType] corresponding to the type string, or null if not found.
+         */
         @JvmStatic
         fun fromType(type: String): SoundType? {
             return entries.find { it.type == type }
