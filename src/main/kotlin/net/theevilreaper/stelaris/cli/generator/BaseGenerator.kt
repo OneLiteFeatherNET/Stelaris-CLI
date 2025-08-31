413f40698e3edd2dd22c0640a8b0244ef3237adc
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-31 16:52:42 +0200

    Remove unused methods

--- src/main/kotlin/net/theevilreaper/stelaris/cli/generator/BaseGenerator.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/BaseGenerator.kt
@@ -1,12 +1,8 @@
 package net.theevilreaper.stelaris.cli.generator
 
-import com.google.common.base.CaseFormat
 import net.theevilreaper.dartpoet.DartFile
 import org.jetbrains.annotations.ApiStatus
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import java.io.IOException
 import java.nio.file.Path
 
 /**
@@ -19,40 +15,10 @@ abstract class BaseGenerator(
     val packageName: String,
 ) : Generator {
 
-    private val emptyMessage: String = "No files to write. Skipping the file write"
-
-    private val logger: Logger = LoggerFactory.getLogger(BaseGenerator::class.java)
     private val filesToGenerate: MutableList<DartFile> = arrayListOf()
 
     private val experimental: Boolean by lazy {
         this.javaClass.isAnnotationPresent(ApiStatus.Experimental::class.java)
-    }
-
-    protected fun writeFiles(filesList: List<DartFile>, outputFolder: Path) {
-        if (filesList.isEmpty()) {
-            logger.info(emptyMessage)
-            return
-        }
-        write(filesList, outputFolder)
-    }
-
-    protected fun writeFiles(outputFolder: Path) {
-        if (filesToGenerate.isEmpty()) {
-            logger.info(emptyMessage)
-            return
-        }
-        write(filesToGenerate, outputFolder)
-        filesToGenerate.clear()
-    }
-
-    private fun write(files: List<DartFile>, outputFolder: Path) {
-        files.forEach { file ->
-            try {
-                file.write(outputFolder)
-            } catch (exception: IOException) {
-                logger.warn("An error occurred while writing source code to the file system: {0}", exception)
-            }
-        }
     }
 
     /**
@@ -61,8 +27,6 @@ abstract class BaseGenerator(
     override fun cleanUp() {
         this.filesToGenerate.clear()
     }
-
-    private fun changeFormat(name: String): String = CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, name)
 
     /**
      * Contains the logic of what happens during the generation.
