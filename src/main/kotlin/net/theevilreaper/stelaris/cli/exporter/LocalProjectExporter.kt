6a05cfa79b3feeb3b78799c1a5d243f9a222c609
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-11-11 08:52:55 +0100

    Improve exporter code

--- src/main/kotlin/net/theevilreaper/stelaris/cli/exporter/LocalProjectExporter.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/exporter/LocalProjectExporter.kt
@@ -1,9 +1,11 @@
 package net.theevilreaper.stelaris.cli.exporter
 
 import net.theevilreaper.stelaris.cli.generator.Generator
+import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
+import java.util.zip.ZipInputStream
 
 class LocalProjectExporter(
     private val exportPath: Path,
@@ -19,17 +21,33 @@ class LocalProjectExporter(
         println("Exporting to local path: $exportPath")
 
         if (!Files.exists(exportPath)) {
-            Files.createDirectories(exportPath)
+            Files.createDirectory(exportPath)
             println("Created directory: $exportPath")
         }
 
-        Files.copy(templateStream, exportPath, StandardCopyOption.REPLACE_EXISTING)
+        println("Exporting to: $exportPath")
+
+        val zipStream = javaClass.getClassLoader().getResourceAsStream("flutter_template.zip")
+        ZipInputStream(zipStream).use { zis ->
+            var entry = zis.nextEntry
+            while (entry != null) {
+                val newPath = exportPath.resolve(entry.name)
+
+                if (entry.isDirectory) {
+                    Files.createDirectories(newPath)
+                } else {
+                    Files.createDirectories(newPath.parent)
+                    Files.copy(zis, newPath, StandardCopyOption.REPLACE_EXISTING)
+                }
+
+                zis.closeEntry()
+                entry = zis.nextEntry
+            }
+        }
 
         val libPath: Path = exportPath.resolve("lib")
         if (!Files.exists(libPath)) Files.createDirectory(libPath)
         modifyPubSpecFile(exportPath, versionString)
-
         generators.forEach { generator -> generator.generate(libPath) }
-
     }
 }
