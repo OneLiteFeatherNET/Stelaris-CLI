642081374928d204a5138056a3cf09627f454052
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-11-11 08:50:02 +0100

    Add a new Gradle task for the template

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,4 +65,15 @@ tasks {
             events("passed", "skipped", "failed")
         }
     }
+
+    processResources {
+        dependsOn("flutter_template_zip")
+    }
+
+    register<Zip>("flutter_template_zip") {
+        exclude("DS_Store")
+        archiveFileName.set("flutter_template.zip")
+        destinationDirectory.set(file("src/main/resources"))
+        from("${rootDir}/assets/flutter_template")
+    }
 }
