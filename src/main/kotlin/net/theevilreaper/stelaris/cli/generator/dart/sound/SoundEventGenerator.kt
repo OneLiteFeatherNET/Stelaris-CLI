8bfe5fb5ab84ec428687f5f92fd0bf83b7f0a9b9
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-11-12 08:57:13 +0100

    Improve sound enum generation

--- src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/sound/SoundEventGenerator.kt
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/sound/SoundEventGenerator.kt
@@ -8,6 +8,7 @@ import net.theevilreaper.dartpoet.clazz.ClassSpec
 import net.theevilreaper.dartpoet.constructor.ConstructorSpec
 import net.theevilreaper.dartpoet.enum.EnumEntrySpec
 import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
+import net.theevilreaper.dartpoet.parameter.ParameterSpec
 import net.theevilreaper.dartpoet.property.PropertySpec
 import net.theevilreaper.stelaris.cli.generator.BaseGenerator
 import net.theevilreaper.stelaris.cli.util.StringHelper
@@ -30,21 +31,25 @@ class SoundEventGenerator : BaseGenerator(
 
             val enumClass = ClassSpec.enumClass(className)
                 .apply {
-                    value.forEach { it ->
-                        val soundKey: Key = it.key()
-                        val soundData = soundKey.value().split(".")
-                        val soundType = soundData[1]
-                        val variant = soundData.last()
-                        val soundTypeTitle = SoundHelper.refactorSoundData(soundType)
-                        val soundVariant = SoundHelper.refactorSoundData(variant)
-                        val displayName = StringHelper.mapDisplayName("$soundTypeTitle $soundVariant")
-                        enumProperty(
-                            EnumEntrySpec.builder("${soundType}_$variant")
-                                .parameter(EnumParameterSpec.positional("%C", displayName))
-                                .parameter(EnumParameterSpec.positional("%C", it.key().asString()))
-                                .build()
-                        )
-                    }
+                    value
+                        .distinctBy { it.key().value().split(".").let { parts -> "${parts[1]}_${parts.last()}" } }
+                        .forEach { it ->
+                            val soundKey: Key = it.key()
+                            val soundData = soundKey.value().split(".")
+                            val soundType = soundData[1]
+                            val variant = soundData.last()
+                            val soundTypeTitle = SoundHelper.refactorSoundData(soundType)
+                            val soundVariant = SoundHelper.refactorSoundData(variant)
+                            val displayName = StringHelper.mapDisplayName("$soundTypeTitle $soundVariant")
+                            val enumName = "${soundType}_$variant"
+
+                            enumProperty(
+                                EnumEntrySpec.builder(enumName)
+                                    .parameter(EnumParameterSpec.positional("%C", displayName))
+                                    .parameter(EnumParameterSpec.positional("%C", it.key().asString()))
+                                    .build()
+                            )
+                        }
                 }
                 .property(
                     PropertySpec.builder("name", String::class).modifier(DartModifier.FINAL).build()
@@ -55,6 +60,8 @@ class SoundEventGenerator : BaseGenerator(
                 .constructor(
                     ConstructorSpec.builder(className)
                         .modifier(DartModifier.CONST)
+                        .parameter(ParameterSpec.positional("name").build())
+                        .parameter(ParameterSpec.positional("key").build())
                         .build()
                 )
                 .build()
