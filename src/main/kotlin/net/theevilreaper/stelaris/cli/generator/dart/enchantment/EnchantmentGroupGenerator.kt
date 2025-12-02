89f33cf9bcf427de79a020f913bbf4a78979c5f0
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-12-02 12:16:05 +0100

    chore(group): add EnchantmentGroup generator

---
+++ src/main/kotlin/net/theevilreaper/stelaris/cli/generator/dart/enchantment/EnchantmentGroupGenerator.kt
@@ -0,0 +1,54 @@
+package net.theevilreaper.stelaris.cli.generator.dart.enchantment
+
+import net.theevilreaper.dartpoet.DartFile
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.clazz.ClassSpec
+import net.theevilreaper.dartpoet.constructor.ConstructorSpec
+import net.theevilreaper.dartpoet.enum.EnumEntrySpec
+import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
+import net.theevilreaper.dartpoet.parameter.ParameterSpec
+import net.theevilreaper.dartpoet.property.PropertySpec
+import net.theevilreaper.stelaris.cli.generator.BaseGenerator
+import net.theevilreaper.stelaris.cli.util.StringHelper
+import java.nio.file.Path
+
+class EnchantmentGroupGenerator: BaseGenerator(
+    packageName = "enchantment",
+    className = "EnchantmentGroup"
+) {
+
+    override fun generate(javaPath: Path) {
+        val enchantmentFolder = javaPath.resolve(packageName)
+        checkPackageFolder(javaPath, packageName)
+
+        val enumClass = ClassSpec.enumClass(className)
+            .apply {
+                EnchantmentGroup.entries.map { entry ->
+                    enumProperty(EnumEntrySpec
+                        .builder(entry.name.lowercase())
+                        .parameter(EnumParameterSpec.positional("%C", StringHelper.mapDisplayName(entry.classPart)))
+                        .build()
+                    )
+                }
+            }
+            .property(PropertySpec.builder("displayName", String::class)
+                .modifier(DartModifier.FINAL)
+                .build()
+            )
+            .constructor(
+                ConstructorSpec.builder(className)
+                    .modifier(DartModifier.CONST)
+                    .parameter(ParameterSpec.positional("displayName").build())
+                    .build()
+            )
+            .build()
+
+        val classFile = DartFile.builder("enchantment_group")
+            .type(enumClass)
+            .build();
+
+        classFile.write(enchantmentFolder)
+    }
+
+    override fun getName(): String = "EnchantmentGroupGenerator"
+}
