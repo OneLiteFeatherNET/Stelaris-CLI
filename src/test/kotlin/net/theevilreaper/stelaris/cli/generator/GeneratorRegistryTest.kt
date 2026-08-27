a726d963583eae2ff8efed955fc364233ca355f6
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-27 20:16:30 +0200

    test(generator): update test case

--- src/test/kotlin/net/theevilreaper/stelaris/cli/generator/GeneratorRegistryTest.kt
+++ src/test/kotlin/net/theevilreaper/stelaris/cli/generator/GeneratorRegistryTest.kt
@@ -9,6 +9,6 @@ class GeneratorRegistryTest {
     fun testGetGenerators() {
         val generatorRegistry = GeneratorRegistry()
         val generators = generatorRegistry.getGenerators()
-        assertEquals(11, generators.size)
+        assertEquals(12, generators.size)
     }
 }
